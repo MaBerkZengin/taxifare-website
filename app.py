a20f25113a02e22b1c8e7e56fa12e4ea6672e2a0
berk zengin <ma.berk.zengin@gmail.com>
2023-03-03 13:41:12 +0800

    first working version 0.0.0.1

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import re
 '''
 # TaxiFareModel User Interface
 '''
-
 status = False
 
 date = st.date_input("Please enter the date like in the format below:",datetime.date(2019, 7, 6))
@@ -17,26 +16,27 @@ datetime_ = str(date) + " " +str(time)
 st.write('Your Datetime is:', datetime_)
 
 
-p_long = st.text_input(f'Please enter the pickup longitude')
-p_lat = st.text_input(f'Please enter the pickup latitude')
-d_long = st.text_input(f'Please enter the dropoff longitude')
-d_lat = st.text_input(f'Please enter the pickup latitude')
-p_count = st.text_input(f'Please enter the numer of passengers')
-
+p_long = st.text_input(f'Please enter the pickup longitude','40.7614327')
+p_lat = st.text_input(f'Please enter the pickup latitude','-73.9898156')
+d_long = st.text_input(f'Please enter the dropoff longitude','40.6513111')
+d_latitude = st.text_input(f'Please enter the pickup latitude','-73.8803531')
+p_count = st.text_input(f'Please enter the numer of passengers','4')
+#p_count = 1
 base_url  = 'https://taxifare.lewagon.ai/predict?'
 
-url = base_url + f'pickup_datetime={datetime_}&pickup_longitude={p_long}&pickup_latitude={p_lat}&dropoff_longitude={d_long}&dropoff_latitude={d_lat}&passenger_count={p_count}'
+url = base_url + f'pickup_datetime={datetime_}&pickup_longitude={p_long}&pickup_latitude={p_lat}&dropoff_longitude={d_long}&dropoff_latitude={d_latitude}&passenger_count={p_count}'
 
 if st.button('Calculate Fare'):
-    print('Initializing....')
+
+    st.write('Calculating..')
     response = requests.get(url).json()
-    print('Response....')
+    st.write(f'Your fare will cost around {round(float(response["fare"]),2)} dollars!')
 
 '''
 # To be deleted
 '''
 
-
+'first working version 0.0.0.1'
 
 st.markdown('''
 Remember that there are several ways to output content into your web page...
@@ -64,11 +64,11 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 '''
 
-url = 'https://taxifare.lewagon.ai/predict'
+#url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
+#if url == 'https://taxifare.lewagon.ai/predict':
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+    #st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
 '''
 
